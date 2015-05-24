84ff61cdef956dc8c217c91c64cc1647f2e762a8
igorpetrov <mail@igorpetrov.com>
2015-05-24 19:51:45 +0300

    Changes

--- Sundry/tvbuild/Gruntfile.js
+++ Sundry/tvbuild/Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
                         '.idea'
                     ]
                 },
-                src: '../tv/',
+                src: '/data/tv/',
                 destDir: '<%= config.tmpDir %>'
             }
         },
